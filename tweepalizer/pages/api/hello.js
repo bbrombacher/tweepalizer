aec56c98f6f9041981a7880f340cca399c7005ad
bbrombacher <bbrombacher@me.com>
2022-11-30 23:15:50 -0800

    get user info

--- tweepalizer/pages/api/hello.js
+++ tweepalizer/pages/api/hello.js
@@ -1,5 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const token = process.env.BEARER_TOKEN;
+const endpointURL = "https://api.twitter.com/2/users/by?usernames="
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const query = req.query;
+    const { user_name } = query
+
+    const userInfo = await fetchUser(user_name)
+    res.status(200).json(userInfo)
+    return
+  }
+}
+
+async function fetchUser(userName) {
+  const response = await fetch(endpointURL+userName, {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+  });
+
+  const data = await response.json()
+  return data
+
 }
